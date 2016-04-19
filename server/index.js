f6107f6d58b6af3d579ede20a0e0a92e7f5fe3ed
Donald Page <don.page.work@gmail.com>
2016-04-19 19:54:23 -0400

    Twilio is reaching server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,34 @@
 "use strict";
-///<reference path="../typings/node/node.d.ts"/>
-///<reference path="../typings/express/express.d.ts"/>
-var secrets_1 = require('./config/secrets');
 var express = require('express');
-var twilio = require('twilio')(secrets_1["default"].sid, secrets_1["default"].secret);
+var bodyParser = require('body-parser');
+var twilio = require('twilio');
 // let resp = new twilio.TwimlResponse();
 var server = express();
+server.use(bodyParser.urlencoded({ extended: false }));
+server.get('/', function (req, res) {
+    res.send('App running on port 3000!');
+});
+server.post('/keyPress', function (req, res) {
+    console.log('req.body ', req.body);
+    var twiml = new twilio.TwimlResponse();
+    twiml.gather({
+        numDigits: '1',
+        action: '/keyPress',
+        method: 'POST',
+        timeout: '1000'
+    });
+    res.send(twiml.toString());
+});
 server.post('/voice', function (req, res) {
-    console.log('POST /voice ', req);
+    console.log('req.body ', req.body);
+    var twiml = new twilio.TwimlResponse();
+    twiml.gather({
+        numDigits: '1',
+        action: '/keyPress',
+        method: 'POST',
+        timeout: '1000'
+    });
+    res.send(twiml.toString());
 });
 server.listen(3000, function () { return console.log('Server on port 3000'); });
 //# sourceMappingURL=index.js.map
--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,49 @@
 ///<reference path="../typings/node/node.d.ts"/>
 ///<reference path="../typings/express/express.d.ts"/>
+///<reference path="../typings/body-parser/body-parser.d.ts"/>
 import config from './config/secrets';
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 
-const twilio = require('twilio')(config.sid, config.secret);
+const twilio = require('twilio');
 // let resp = new twilio.TwimlResponse();
 
 
-
 const server = express();
+server.use(bodyParser.urlencoded({extended: false}));
+
+server.get('/', (req, res) => {
+  res.send('App running on port 3000!');
+});
+
+server.post('/keyPress', (req, res) => {
+  console.log('req.body ', req.body);
+  let twiml = new twilio.TwimlResponse();
+  twiml.gather({
+    numDigits: '1',
+    action: '/keyPress',
+    method: 'POST',
+    timeout: '1000'
+  });
+
+  res.send(twiml.toString());
+});
 
 server.post('/voice', (req, res) => {
-  console.log('POST /voice ', req);
+  console.log('req.body ', req.body);
+  let twiml = new twilio.TwimlResponse();
+  twiml.gather({
+    numDigits: '1',
+    action: '/keyPress',
+    method: 'POST',
+    timeout: '1000'
+  });
+
+  res.send(twiml.toString());
 });
+
+
 
 server.listen(3000, () => console.log('Server on port 3000'));
 
