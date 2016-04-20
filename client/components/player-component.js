7f4c96463b06dae897d9589f27c21dfe35e19c54
ptlergo <ptlergo@gmail.com>
2016-04-19 20:50:29 -0400

    player circle made

---
+++ client/components/player-component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Player extends React.Component {
+  render() {
+    return <div id="player"></div>;
+  }
+}
+
+// Export dependencies like this:
+export default Player;
--- client/index.js
+++ client/index.js
@@ -7,13 +7,16 @@ import './styles/main.scss';
 
 // Import dependencies like this:
 import Goat from './components/goat-component';
+import Player from './components/player-component';
 
 class App extends React.Component {
   render() {
     return (
-      <div>I heard React was good. <Goat /></div>
+      <div>I heard React was good. <Goat /><Player /></div>
     );
   }
 }
 
+
 ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('player'));
