fce67973cc0c5761f904ffcdc451fe5af68d12e5
checkkdahustle <checkkdahustle@fullsail.edu>
2016-04-20 01:28:29 -0400

    made a separate repo for this react file from Don Page, from hack night

--- client/components/player-component.js
+++ client/components/player-component.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 class Player extends React.Component {
   render() {
-    return <div id="player"></div>;
+    return <div id="player">
+      <div class="van move-right"></div>
+    </div>;
   }
 }
 
 // Export dependencies like this:
 export default Player;
+
+// circle top -1
+//   start game board x,y 0,0
